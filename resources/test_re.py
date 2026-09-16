769c209bbfe0cd977cd1bf1d1e583e73847e4a32
Mahdi L'Haouari <mal-haou@c2r1p12.1337.ma>
2026-09-16 14:32:37 +0100

    adding same tests in re subfolder

--- resources/test_re.py
+++ resources/test_re.py
@@ -1,6 +1,74 @@
 import re
 
-text = "nb of dranes: "
+# text = "nb of dranes: --32"
 
-result = re.fullmatch(r"nb of dranes: \d?", text)
-print(result)
+# result = re.fullmatch(r"nb of dranes: (--?\d+)", text)
+# print(result.group(1))
+
+
+# result2 = re.findall(r"[^a-b]+", "dwdazwwdwdazwd")
+# print(result2(1))
+
+
+
+
+
+
+# if result.group(2) in [z.value for z in ZoneType]:
+#     print("done")
+
+
+from enum import Enum
+
+class ZoneType(Enum):
+    NORMAL = "normal"
+    BLOCKED = "blocked"
+    RESTRICTED = "restricted"
+    PRIORITY = "priority"
+
+line = "start_hub: normal 0 0 [color=green zone=restricted max_drones=2 ]"
+
+try:
+    result = re.fullmatch(r"(start_hub|end_hub|hub): ([^\s-]+) (?P<x>-?\d+) (?P<y>-?\d+) (\[.*\])?", line)
+    print(result.group(0))
+except (AttributeError, IndexError) as e:
+    print( f"wa {e}")
+
+
+# hub type
+
+# name
+
+# x
+
+# y
+
+# metadata
+
+result2 = re.findall(r"(\w+)=(\S+)", result.group(5))
+print(result2)
+
+dic = {}
+for x, y in result2:
+    dic[x] = y
+
+allowed_metadata = {"zone", "max_drones", "color"}
+
+# Unknown metadata
+unknown = set(dic) - allowed_metadata
+
+if unknown:
+    print(f"metadata not recognized: {unknown}")
+
+
+if dic["zone"] in [z.value for z in ZoneType]:
+    value = dic["zone"]
+print(value)
+
+nb_drones = int(dic["max_drones"])
+print(nb_drones)
+
+color = dic["color"]
+
+print(color)
+
--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 # a test that show that when we not initialise the objects created 
 # from the same class the store the data in the same object
 class test:
-    def __init__(self, list: list = []) -> None:
+    l = [1]
+    def __init__(self, list = []) -> None:
         self.list = list
 
 
 test1 = test()
 test2 = test()
-
-print(f"{test1.list} AND {test2.list}")
+print(test.l)
+print(f"{id(test1.list)} AND {id(test2.list)}")
 
 print(test1.list is test2.list)
 test1.list.append(3)
 
 print(f"{test1.list} AND {test2.list}")
+from collections import OrderedDict
+
+a = OrderedDict()
+a.
